b2ce03dd7cde8808565f08e4c6d8bc76cb1c866a
Valerie <412963@niuitmo.ru>
2025-02-18 09:36:38 +0100

    add 3.sh solution

--- lab-1/3.sh
+++ lab-1/3.sh
@@ -1,3 +1,35 @@
 #! /bin/bash
 # Создать текстовое меню с четырьмя пунктами. При вводе пользователем номера пункта меню
-происходит запуск редактора nano, редактора vi, браузера links или выход из меню.
+#происходит запуск редактора nano, редактора vi, браузера links или выход из меню.
+
+menu="choose the option:
+    1. open nano
+    2. open vi
+    3. open links browser
+    4. exit"
+echo "$menu"
+
+while true;
+do 
+    read option
+    case $option in
+        1)
+            nano
+            ;;
+        2) 
+            vi
+            ;;
+        3)
+            links
+            ;;
+        4)
+            break
+            ;;
+        *)
+            echo "invalid input."
+            echo "$menu"
+            ;;
+    esac
+done
+
+
