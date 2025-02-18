a18ef25b2033024f89a0f572eaa5c7f99819588b
Valerie <412963@niuitmo.ru>
2025-02-18 12:44:56 +0100

    fix typo

--- lab-1/5.sh
+++ lab-1/5.sh
@@ -3,7 +3,7 @@
 # или, если таковой отсутствует, /var/log/installer/syslog второе поле в которых равно INFO
 
 dir1="/var/log/anaconda/syslog"
-dir2="var/log/installer/syslog"
+dir2="/var/log/installer/syslog"
 
 if [ -f "$dir1" ]; then
     awk '$2 == "INFO"' "$dir1" > info.log
