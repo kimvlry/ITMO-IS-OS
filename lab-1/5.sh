8c6faea9e72342f50daff1baff3d30159688645e
Valerie <412963@niuitmo.ru>
2025-02-18 12:07:06 +0100

    add 5.sh solution

--- lab-1/5.sh
+++ lab-1/5.sh
@@ -1,3 +1,14 @@
 #! /bin/bash
 # Создать файл info.log, в который поместить все строки из файла /var/log/anaconda/syslog
-или, если таковой отсутствует, /var/log/installer/syslog второе поле в которых равно INFO.
+# или, если таковой отсутствует, /var/log/installer/syslog второе поле в которых равно INFO
+
+dir1="/var/log/anaconda/syslog"
+dir2="var/log/installer/syslog"
+
+if [ -f "$dir1" ]; then
+    awk '$2 == "INFO"' "$dir1" > info.log
+elif [ -f "$dir2" ]; then
+    awk '$2 == "INFO"' "$dir2" > info.log
+else 
+    echo "none of needed directories were found"
+fi
