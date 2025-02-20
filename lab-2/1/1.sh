184b8329569a4a223e4e34372a3b39d57bf491a9
Valerie <412963@niuitmo.ru>
2025-02-20 10:42:41 +0100

    add 1.sh solution

--- lab-2/1/1.sh
+++ lab-2/1/1.sh
@@ -2,3 +2,8 @@
 # в файл получившееся число, а затем пары PID:команда для таких процессов.
 #
 #! /bin/bash
+
+echo "enter user name"
+read user
+ps -u $user --no-headers | wc -l > output.txt && \
+ps -u $user -o pid,cmd --no-headers | awk '{print $1 ":" $2}' >> output.txt
