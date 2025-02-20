666e9b94ef10406f39d483ecd9af2756fff033a3
Valerie <412963@niuitmo.ru>
2025-02-20 11:23:24 +0100

    add 3.sh solution

--- lab-2/3.sh
+++ lab-2/3.sh
@@ -1,3 +1,5 @@
 # Вывести на экран PID процесса, запущенного последним (с последним временем запуска).
 #
 #! /bin/bash
+
+ps e -o pid,lstart --no-headers | sort -k6 -k3,4 -k5 | tail -n1 | awk '{print $1}'
