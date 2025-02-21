332550865c49973a736a7adea76af912cdda2e7c
Valerie <412963@niuitmo.ru>
2025-02-21 23:11:45 +0100

    add 6.sh solution

--- lab-2/6.sh
+++ lab-2/6.sh
@@ -2,3 +2,30 @@
 # больше всего оперативной памяти. Сравнить результат с выводом команды top.
 #
 #! /bin/bash
+
+max_memo=-1
+max_memo_process_name=""
+max_memo_processID=-1
+
+for pid in /proc/[0-9]*; do
+    pid=${pid#/proc/}
+
+    if [[ -f "/proc/$pid/status" ]]; then
+        status=/proc/$pid/status
+        name=$(grep "^Name:" $status | awk '{print $2}')
+        memo=$(grep "^VmRSS:" $status | awk '{print $2}')
+
+        if [[ -n $memo && $memo -gt $max_memo ]]; then
+            max_memo=$memo
+            max_memo_process_name=$name
+            max_memo_processID=$pid
+        fi
+    fi
+done
+
+echo "according to /proc:"
+echo -e "$max_memo_processID $max_memo_process_name $max_memo kB\n"
+
+echo "according to top command:"
+top -b -o %MEM | head -n 8 | tail -n 2
+
