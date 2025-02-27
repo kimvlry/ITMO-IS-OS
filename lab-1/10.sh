bef14404d6ae8c3f8dc98c99bac94698ccf102d4
Kim Valerie <412963@niuitmo.ru>
2025-02-27 16:23:12 +0300

    Update 10.sh

--- lab-1/10.sh
+++ lab-1/10.sh
@@ -3,3 +3,12 @@
 # по команде bash длиной не менее четырех символов.
 
 man -T ascii bash | tr -cs '[:alpha:]' '\n' | tr '[:upper:]' '[:lower:]' | grep -E '^[a-zA-Z]{4,}$' | sort | uniq -c | sort -rn | head -3
+
+# -c compliment set
+# -s squeeze (to skip empty strings)
+#
+# uniq - removes the duplications of a line BUT duplicated lines must be adjacent
+# -c count (adds counter before every unique word)
+#
+# -r reverse 
+# -n numeric
