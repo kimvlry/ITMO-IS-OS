84c415511f59f7c7f794a09393194bdbf1bb2671
Valerie <412963@niuitmo.ru>
2025-02-27 19:47:50 +0100

    fix 7.sh

--- lab-1/7.sh
+++ lab-1/7.sh
@@ -2,11 +2,10 @@
 # Создать файл emails.lst, в который вывести через запятую все адреса электронной почты,
 # встречающиеся во всех файлах директории /etc.
 
-regex="[a-zA-Z0-9._-]+@[a-z0-9_-]+\.[a-z]{2,}"
-grep -roE "$regex" /etc | uniq | tr '\n' ',' | sed 's/,$/\n/' > email.lst
+regex="[[:alnum:]._]+@[[:alnum:]._]+\.[[:alnum:]]{2,}"
+grep -rIEoh "$regex" /etc | sort -u | tr '\n' ',' > email.lst
 
 # -r for recursive search in /etc
-# -o return only occurrences and not the whole line
 # -E extended regex syntax
 #
 # sed goes line by line and tr reads char by char
