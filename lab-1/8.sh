d6de8b1cc994566ebf22753082444a19ef8b71c7
Kim Valerie <412963@niuitmo.ru>
2025-02-27 16:07:00 +0300

    Update 8.sh

--- lab-1/8.sh
+++ lab-1/8.sh
@@ -4,3 +4,8 @@
 # первое поле – имя пользователя, третье поле – UID. Разделитель – двоеточие.
 
 cut -d ':' -f1,3 --output-delimiter=' ' /etc/passwd | sort -k2 -n 
+
+# -d delimiter to cut on
+# -f1,3 field 1 and 3
+# -k2 sorting field
+# -n numerical sort
