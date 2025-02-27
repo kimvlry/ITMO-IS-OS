4d178e527e019202cadf698f9d5b831c307745c4
Kim Valerie <412963@niuitmo.ru>
2025-02-27 16:03:09 +0300

    Update 7.sh

--- lab-1/7.sh
+++ lab-1/7.sh
@@ -3,4 +3,12 @@
 # встречающиеся во всех файлах директории /etc.
 
 regex="[a-zA-Z0-9._-]+@[a-z0-9_-]+\.[a-z]{2,}"
-grep -roE "$regex" /etc | uniq | tr '\n' ',' | sed 's/,$/\n/' | > email.lst
+grep -roE "$regex" /etc | uniq | tr '\n' ',' | sed 's/,$/\n/' > email.lst
+
+# -r for recursive search in /etc
+# -o return only occurrences and not the whole line
+# -E extended regex syntax
+#
+# sed goes line by line and tr reads char by char
+#
+# s/../.. - sed substitution ($ stand for end of the line)
