8c8d92520158ac41e43450373caf6724bba7ba78
Kim Valerie <412963@niuitmo.ru>
2025-02-27 16:12:46 +0300

    Update 9.sh

--- lab-1/9.sh
+++ lab-1/9.sh
@@ -3,3 +3,9 @@
 # в директории /var/log/ и имеющих расширение log.
 
 find /var/log/ -type f -name "*.log" -exec wc -l {} + | awk '{count+=$1} END {print count}'
+
+# find <direction> <conditions> -exec <command> {} - apply command for every found file
+# wc word count 
+# -l lines 
+# + pass several files at once
+
