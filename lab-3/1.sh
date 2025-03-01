1b820a69d1107f75ab75aca0712bfb18081086bf
Valerie <412963@niuitmo.ru>
2025-03-01 21:20:47 +0100

    fix 1.sh solution

--- lab-3/1.sh
+++ lab-3/1.sh
@@ -5,7 +5,6 @@ timestamp=$(date +"%Y-%m-%d_%H:%M:%S")
 mkdir $HOME/test 2>/dev/null && {
     echo 'catalog test was created successfully' >> ~/report;
     touch "$HOME/test/$timestamp";
-    echo "$timestamp"
 };
 
 ping -c 1 www.net_nikogo.ru >/dev/null 2>&1 || echo "$timestamp host is unreachable" >> ~/report
