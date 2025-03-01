1c73e2179c14ecf1415a547f5982169a62497227
Valerie <412963@niuitmo.ru>
2025-03-01 21:37:51 +0100

    fix 1.sh solution

--- lab-3/1.sh
+++ lab-3/1.sh
@@ -3,11 +3,11 @@
 timestamp=$(date +"%Y-%m-%d_%H:%M:%S")
 
 mkdir $HOME/test 2>/dev/null && {
-    echo 'catalog test was created successfully' >> ~/report;
+    echo 'catalog test was created successfully' >> $HOME/report;
     touch "$HOME/test/$timestamp";
 };
 
-ping -c 1 www.net_nikogo.ru >/dev/null 2>&1 || echo "$timestamp host is unreachable" >> ~/report
+ping -c 1 www.net_nikogo.ru >/dev/null 2>&1 || echo "$timestamp host is unreachable" >> $HOME/report
 
 # >2/dev/null: 2 stands for stderr
-# 2>&1: redirect stderr contents to stdout (1)
+# 2>&1: redirect stderr contents to the stdout
