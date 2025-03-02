d5c5fe318bc566152a3377f069cf6a8d892489ed
Valerie <412963@niuitmo.ru>
2025-03-02 18:34:37 +0100

    add 5 solution

--- lab-3/5/generator.sh
+++ lab-3/5/generator.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 
+PIPE="../pipe"
+
+echo "select mode ('+' for addititon or '*' for multiplication),"
+echo "input numerical operand (basic mode is '+')"
+echo "or type 'QUIT' to exit app"
+
+while true; do
+    read INPUT
+    if [[ "$INPUT" =~ ^[0-9]+$ ]] || [[ "$INPUT" == "+" ]] || [[ "$INPUT" == "*" ]]; then
+        echo "$INPUT" > $PIPE
+    elif [[ "$INPUT" == "QUIT" ]]; then
+        echo "QUIT" > $PIPE
+        echo "quitting generator"
+        break
+    else 
+        echo "INVALID" > $PIPE
+        echo "quitting generator"
+        break
+    fi
+done
+
--- lab-3/5/handler.sh
+++ lab-3/5/handler.sh
@@ -1,1 +1,37 @@
 #!/bin/bash
+
+PIPE="../pipe"
+MODE="+"
+NUM=1
+
+while true; do
+    read INPUT < $PIPE
+    if [[ "$INPUT" == "INVALID" ]] || [[ "$INPUT" == "QUIT" ]] ; then
+        echo "quitting handler"
+        break
+    else
+        case $INPUT in
+            "+")
+                MODE="+"
+                ;;
+            "*")
+                MODE="*"
+                ;;
+            *)
+                case $MODE in 
+                    "+")
+                        NUM=$((NUM+INPUT))
+                        echo "$NUM"
+                        ;;
+                    "*")
+                        NUM=$((NUM*INPUT))
+                        echo "$NUM"
+                        ;;
+                    *)
+                        ;;
+                esac
+        esac
+    fi
+done
+
+
