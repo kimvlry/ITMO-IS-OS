e69573c7fd97d75aca0e10e8edf4080fcc1c22d6
Valerie <412963@niuitmo.ru>
2025-03-02 19:16:23 +0100

    add 6 solution

--- lab-3/6/generator.sh
+++ lab-3/6/generator.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+HANDLER_PID=$(cat handlerPID.txt)
+
+echo "select mode ('+' for addititon or '*' for multiplication),"
+echo "or type 'TERM' to exit app"
+
+while true; do
+    read INPUT
+    case $INPUT in
+        "+")
+            kill -USR1 "$HANDLER_PID"
+            ;;
+        "*")
+            kill -USR2 "$HANDLER_PID"
+            ;;
+        "TERM")
+            kill -TERM "$HANDLER_PID"
+            ;;
+        *)
+            ;;
+    esac
+done
+
--- lab-3/6/handler.sh
+++ lab-3/6/handler.sh
@@ -1,1 +1,31 @@
 #!/bin/bash
+
+echo $$ > "handlerPID.txt"
+MODE="+"
+NUM=1
+
+addition() { MODE="+" }
+multiplication() { MODE="*" }
+termination() {
+    echo "quitting handler"
+    exit 0
+}
+
+trap 'addition' USR1
+trap 'multiplication' USR2
+trap 'termination' TERM
+
+while true; do
+    sleep 1
+
+    case "$MODE" in 
+        "+")
+            NUM=$((NUM+2))
+            ;;
+        "*")
+            NUM=$((NUM*2))
+            ;;
+        *)
+            ;;
+    esac
+done
