845406b291efa976656c7f0e6f25c2cccd1149f0
Valerie <412963@niuitmo.ru>
2025-03-02 19:31:50 +0100

    fix limiter

--- lab-3/4/limiter.sh
+++ lab-3/4/limiter.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
+
 if [ ! -f "PIDs.txt" ]; then
     echo "no PIDs.txt"
     exit 1
 fi
 
 first=$(head -n 1 "PIDs.txt")
-third=$(head -n 3 "PIDs.txt")
+third=$(sed -n '3p' "PIDs.txt")
 
-if [ ! ps -p "$first" ] >/dev/null; then
+if ! ps -p "$first" >/dev/null; then
     echo "first proccess not found"
     exit 1
 fi
 
-if [ ! ps -p "$third" ] >/dev/null; then
+if ! ps -p "$third" >/dev/null; then
     echo "third proccess not found"
     exit 1
 fi
 
 percent_limit=10
 while true; do
-    cpu_usage=$(ps -p "$first" -o %cpu --no-headers)
-    if (( $(bc <<< "$cpu_usage > $percent_limit") )); then
-        renice -n 10 -p "$first"
+    cpu_usage=$(ps -p "$first" -o %cpu --no-headers | awk '{print $1}')
+    if [[ -n "$cpu_usage" $(echo "$cpu_usage > $percent_limit" | bc) -eq 1 ]]; then
+        echo "current cpu usage = $cpu_usage , lowering priority"
+        sudo renice -n 10 -p "$first" > /dev/null
+        sleep 3
     else 
-        echo "all good"
+        echo "current CPU usage = $cpu_usage , all good"
         break
     fi
 done
-echo "less 10"
+echo "resulting CPU usage = $cpu_usage"
 
 kill -9 "$third"
 echo "killed 3"
