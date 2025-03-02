e5f67830b58192a9bd1b33b49072d2b2cc503622
Valerie <412963@niuitmo.ru>
2025-03-02 15:11:26 +0100

    fix 1.sh

--- lab-3/1.sh
+++ lab-3/1.sh
@@ -2,12 +2,13 @@
 
 timestamp=$(date +"%Y-%m-%d_%H:%M:%S")
 
-mkdir $HOME/test 2>/dev/null && {
-    echo 'catalog test was created successfully' >> $HOME/report;
+HOME="/home/valerie"
+mkdir "$HOME"/test 2>/dev/null && {
+    echo 'catalog test was created successfully' >> "$HOME"/report;
     touch "$HOME/test/$timestamp";
 };
 
-ping -c 1 www.net_nikogo.ru >/dev/null 2>&1 echo "ping" || echo "write" echo "$timestamp host is unreachable" >> $HOME/report
+ping -c 1 www.net_nikogo.ru >/dev/null 2>&1 || echo "$timestamp host is unreachable" >> "$HOME"/report
 
 # >2/dev/null: 2 stands for stderr
 # 2>&1: redirect stderr contents to the stdout
