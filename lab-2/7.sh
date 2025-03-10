263ac629a98f7a3dbcf5908a171da434aa96ca7a
Kim Valerie <412963@niuitmo.ru>
2025-03-10 20:58:33 +0300

    Update 7.sh

--- lab-2/7.sh
+++ lab-2/7.sh
@@ -11,10 +11,12 @@ collect_io() {
     # declaring a link to the array passed to the function (copying array would be: declare n=("${!1[@]}"))
     declare -n arr=$1 
     while read -r filename bytes; do
-        pid=${filename//[^0-9]/}
+        # delete non-numeric symbols
+        pid=${filename//[^0-9]/} 
         if [[ -d "/proc/$pid" ]]; then
             arr[$pid]=$bytes
         fi 
+    # NF is the last value in string (read_bytes) 
     done < <(awk '/read_bytes/ {print FILENAME, $NF}' /proc/[0-9]*/io 2>/dev/null)
 }
 
@@ -22,17 +24,13 @@ collect_io before
 sleep 60
 collect_io after
 
-# for all keys
-for pid in ${!before[@]}; do
-    if [[ -n ${after[$pid]} ]]; then
-        diff[$pid]=$(( after[$pid] - before[$pid] ))
-    else 
-        diff[$pid]=${before[$pid]}
-    fi
+for pid in "${!before[@]}"; do
+    # if process was terminated between checks, the after value is 0 (:-0)
+    diff["$pid"]=$(( ${after[$pid]:-0} - ${before[$pid]} ))
 done
 
-if [[ ${diff[@]} -eq 0 ]]; then
-    echo "no proccesses read disk"
+if [[ ${#diff[@]} -eq 0 ]]; then
+    echo "No processes with disk read activity detected!"
     exit 0
 fi
 
@@ -41,7 +39,7 @@ fi
 
 # inner: for all keys (PIDs): echo values (bytes) and sort 'em, increasing
 # outer: takes top 3 PIDs (`cut -d' '-f1`) of inner loop
-for pid in $(for p in "${!diff[@]}"; do echo $p "${diff[$p]}"; done | sort -nr -k2 | head -n 3 | cut -d' ' -f1); do
+for pid in $(for p in "${!diff[@]}"; do echo $p "${diff[$p]}"; done | sort -rn -k2 | head -n 3 | cut -d' ' -f1); do
     cmd=$(ps -o cmd= -p "$pid" 2>/dev/null);
     [[ -z $cmd ]] && cmd="[terminated]"
     echo "$pid : $cmd : ${diff[$pid]}";
