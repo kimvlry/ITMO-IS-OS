de8cdfdc5158b770ca45a74b953cc3aef5e87ec1
Valerie <412963@niuitmo.ru>
2025-03-13 17:32:30 +0100

    add extra task

---
+++ lab-3/extra_task/second.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+PIPE="pipe"
+if [ ! -p "$PIPE" ]; then
+    mkfifo "$PIPE"
+fi
+
+read() {
+    if read message <"$PIPE"; then 
+        if [[ $message == "TERM" ]]; then       
+            echo "they exit chat"
+            break
+        fi
+        echo "THEM: $message"
+    fi    
+}
+
+write() {
+    echo "enter your message:"
+    read message
+    if [[ $message == "TERM" ]]; then
+        echo $message>$PIPE
+        break
+    fi
+    echo "$message">$PIPE
+}    
+
+echo "USER2 is connected">$PIPE
+echo "enter TERM to exit chat"
+
+while true; do
+    write
+    read
+done 
+
+rm -rf $PIPE
