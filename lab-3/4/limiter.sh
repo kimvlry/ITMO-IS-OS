ea36ebb09cf82f21fa7976f63e39fff12d8f0f2d
Valerie <412963@niuitmo.ru>
2025-03-13 12:23:35 +0100

    update limiter.sh

--- lab-3/4/limiter.sh
+++ lab-3/4/limiter.sh
@@ -13,15 +13,8 @@ fi
 first=$(head -n 1 "PIDs.txt")
 third=$(sed -n '3p' "PIDs.txt")
 
-if ! ps -p "$first" >/dev/null; then
-    echo "first process not found"
-    exit 1
-fi
-
-if ! ps -p "$third" >/dev/null; then
-    echo "third process not found"
-    exit 1
-fi
+ps -p "$first" || { echo "first process not found"; exit 1; }
+ps -p "$third" || { echo "third process not found"; exit 1; }
 
 percent_limit=10
 {
@@ -30,19 +23,14 @@ percent_limit=10
         cpu_usage=${cpu_usage%.*}
         current_nice=$(top -bn1 -p $first | tail -n +8 | awk '{print $4}')
 
-        if [[ $cpu_usage -gt 10 ]] && [[ $current_nice -lt 19 ]]; then
-            echo "first process CPU usage: ${cpu_usage}% -> increasing nice by 1 from $current_nice..."
+        if [[ $cpu_usage -gt $percent_limit ]] && [[ $current_nice -lt 19 ]]; then
             renice -n $((current_nice + 1)) -p $first
-        else
-            echo "current CPU usage: $cpu_usage%"
-        fi
-        sleep 2
+        fi 
+        echo "current CPU usage: $cpu_usage%"
+        sleep 5
     done
 } & 
 
-kill "$third"
+kill "$third" >/dev/null
 echo "killed process 3"
 
-sleep 2
-echo -e "\ncurrent CPU usage of process 1:"
-top -bn1 -p $first | tail -n +8 | awk '{printf "PID: %s\nNI: %s\nCPU: %s%%\n", $1, $4, $9}'
