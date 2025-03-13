4e739a92d4fd79803d7db45d7891a8f8c481cdcc
Kim Valerie <412963@niuitmo.ru>
2025-03-13 17:47:56 +0300

    Update limiter.sh

--- lab-3/4/limiter.sh
+++ lab-3/4/limiter.sh
@@ -7,33 +7,29 @@
 
 if [ ! -f "PIDs.txt" ]; then
     echo "no PIDs.txt"
-    exit
+    exit 1
 fi
 
 first=$(head -n 1 "PIDs.txt")
 third=$(sed -n '3p' "PIDs.txt")
-ps -p "$first" || { echo "first PID not found"; exit }
-ps -p "$third" || { echo; "third PID not found"; exit }
+
+ps -p "$first" || { echo "first process not found"; exit 1; }
+ps -p "$third" || { echo "third process not found"; exit 1; }
 
 percent_limit=10
-
 {
     while true; do
         cpu_usage=$(top -bn1 -p $first | tail -n +8 | awk '{print $9}')
         cpu_usage=${cpu_usage%.*}
         current_nice=$(top -bn1 -p $first | tail -n +8 | awk '{print $4}')
-        
+
         if [[ $cpu_usage -gt $percent_limit ]] && [[ $current_nice -lt 19 ]]; then
-            echo "first proccess CPU usgae: ${cpu_usage} -> increasing nice by 1 from $current_nice..."
             renice -n $((current_nice + 1)) -p $first
-        else
-            echo "current CPU usage: $cpu_usage"
-            break;
-        fi
-        
-        sleep 2
+        fi 
+        echo "current CPU usage: $cpu_usage%"
+        sleep 5
     done
 } & 
 
-kill -9 "$third"
-echo "killed proccess 3"
+kill "$third" >/dev/null
+echo "killed process 3"
