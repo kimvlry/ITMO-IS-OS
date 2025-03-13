1903fad8b785be48c8d94a4890d42f7d9d71a1a9
Kim Valerie <412963@niuitmo.ru>
2025-03-13 18:14:51 +0300

    Update 3.sh

--- lab-3/3.sh
+++ lab-3/3.sh
@@ -1,4 +1,4 @@
 # Задайте запуск скрипта из пункта 1 в каждую пятую минут каждого часа в день недели, 
 # в который вы будете выполнять работу.
 
-( crontab -l; echo "1 * * * THU /home/valerie/ITMO-IS-OS/lab-3/1.sh" ) | crontab
+( crontab -l; echo "5 * * * SUN /home/valerie/ITMO-IS-OS/lab-3/1.sh" ) | crontab
