1b671989c9bf0753daa2b67059947f5201f6b470
Kim Valerie <412963@niuitmo.ru>
2025-03-13 17:40:43 +0300

    Update limiter.sh

--- lab-3/4/limiter.sh
+++ lab-3/4/limiter.sh
@@ -6,31 +6,28 @@
 #!/bin/bash
 
 if [ ! -f "PIDs.txt" ]; then
-    echo "no PIDs.txt"
-    exit 1
+    if ! ps -p "$third" >/dev/null; then
 fi
 
-first=$(head -n 1 "PIDs.txt")
-third=$(sed -n '3p' "PIDs.txt")
-
-ps -p "$first" || { echo "first process not found"; exit 1; }
-ps -p "$third" || { echo "third process not found"; exit 1; }
-
 percent_limit=10
+
 {
     while true; do
-        stats=$(top -bn1 -p "$1" | grep -E "^[ ]*$1 ")
-        cpu_usage=$(echo "$stats" | awk '{printf "%.0f", $9}')
-        current_nice=$(echo "$stats" | awk '{print $4}')
-
+        cpu_usage=$(top -bn1 -p $first | tail +8 | awk '{print $9}')
+        cpu_usage=${cpu_usage%.*}
+        current_nice=$(top -bn1 -p $first | tail -n +8 | awk '{print $4}')
+        
         if [[ $cpu_usage -gt $percent_limit ]] && [[ $current_nice -lt 19 ]]; then
+            echo "first proccess CPU usgae: ${cpu_usage} -> increasing nice by 1 from $current_nice..."
             renice -n $((current_nice + 1)) -p $first
-        fi 
-        echo "current CPU usage: $cpu_usage%"
-        sleep 5
+        else
+            echo "current CPU usage: $cpu_usage"
+            break;
+        fi
+        
+        sleep 2
     done
 } & 
 
-kill "$third" >/dev/null
-echo "killed process 3"
-
+kill -9 "$third"
+echo "killed proccess 3"
