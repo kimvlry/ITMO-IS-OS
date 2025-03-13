0b531450db27bb6f42925514bf61304220a59710
Kim Valerie <412963@niuitmo.ru>
2025-03-13 20:20:20 +0300

    Update first.sh

--- lab-3/extra_task/first.sh
+++ lab-3/extra_task/first.sh
@@ -5,38 +5,35 @@ if [ ! -p "$PIPE" ]; then
     mkfifo "$PIPE"
 fi
 
-trap 'rm -rf "$PIPE"' EXIT
+trap 'rm -f "$PIPE"; exit' EXIT SIGINT
 
-read_message() {
+read_messages() {
     while true; do
-        if read -t 1 message <"$PIPE"; then  
-            if [[ $message == "TERM" ]]; then
-                echo "USER 2 exits chat."
+        if read -t 1 message <"$PIPE"; then
+            if [[ "$message" == "TERM" ]]; then
+                echo "USER2 exited the chat."
                 break
             fi
-            echo "USER 2: $message"
+            echo "USER2: $message"
         fi
     done
-    exit 0
 }
 
-write_message() {
+send_messages() {
     while true; do
-        echo -n "enter message: "
+        echo -n "Your message: "
         read message
-        if [[ $message == "TERM" ]]; then
+        if [[ "$message" == "TERM" ]]; then
             echo "$message" >"$PIPE"
             break
         fi
         echo "$message" >"$PIPE"
     done
-    exit 0
 }
 
-echo "chat is running"
-echo "enter 'TERM' to exit"
+echo "Chat started. Type 'TERM' to exit."
 
-read_message &
-write_message &
+read_messages &
+send_messages &
 
 wait
