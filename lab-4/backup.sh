6c11aa3ba57185171e9f871e9e1c385a90e217d3
Valerie <412963@niuitmo.ru>
2025-03-16 14:53:17 +0100

    upd

--- lab-4/backup.sh
+++ lab-4/backup.sh
@@ -14,22 +14,29 @@
 # c. Если каталог не был создан (есть «действующий» каталог резервного копирования), то скрипт
 #    должен скопировать в него все файлы из /home/user/source/ по следующим правилам: если
 #    файла с таким именем в каталоге резервного копирования нет, то он копируется из
-#    /home/user/source. Если файл с таким именем есть, то его размер сравнивается с размером
-#    одноименного файла в действующем каталоге резервного копирования. Если размеры совпадают,
-#    файл не копируется. Если размеры отличаются, то файл копируется c автоматическим созданием
+#    /home/user/source. 
+#    Если файл с таким именем есть, то его размер сравнивается с размером
+#    одноименного файла в действующем каталоге резервного копирования. 
+#    Если размеры совпадают, файл не копируется. 
+#    Если размеры отличаются, то файл копируется c автоматическим созданием
 #    версионной копии, таким образом, в действующем каталоге резервного копирования появляются
 #    обе версии файла (уже имеющийся файл переименовывается путем добавления дополнительного
-#    расширения «.YYYY-MM-DD» (дата запуска скрипта), а скопированный сохраняет имя). После
-#    окончания копирования в файл /home/user/backup-report выводится строка о внесении
-#    изменений в действующий каталог резервного копирования с указанием его имени и даты
-#    внесения изменений, затем строки, содержащие имена добавленных файлов с новыми именами, 
-#    а затем строки с именами добавленных файлов с существовавшими в действующем каталоге
+#    расширения «.YYYY-MM-DD» (дата запуска скрипта), а скопированный сохраняет имя). 
+#
+#    После окончания копирования в файл /home/user/backup-report выводится строка о внесении
+#    изменений в действующий каталог резервного копирования с указанием 
+#    - его имени и даты внесения изменений
+#    - строки, содержащие имена добавленных файлов с новыми именами
+#    - строки с именами добавленных файлов с существовавшими в действующем каталоге
 #    резервного копирования именами с указанием через пробел нового имени, присвоенного
 #    предыдущей версии этого файла
 
 #!/bin/bash
 
 # a
+echo "input source folder name"
+read source
+
 today=$(date +"%Y-%m-%d")
 expired_date=$(date -I -d "$timestamp - 7 days")
 backup=0
@@ -44,18 +51,46 @@ while [ $current != $expired_date ]; do
     current=$(date -I -d "$current - 1 day")
 done;
 
-source_path="~/home/user/source/"
-backup_report="~/home/user/backup-report"
-# b
+source_path="~/${source}/"
+backup_report="~/backup-report"
+
+if [ ! -f $backup_report ]; then
+    touch $backup_report
+fi
+
 if [ $backup == 0 ]; then
     backup="Backup-${today}"
-    mkdir ~/home/user/"$backup
-    backup_report < "${today} : Created backup file ${backup}"
-    cp -rv "$source_path" ~/home/user/"$backup" | awk -F"'" '{print $2}' >> $backup_report
+    mkdir "~/$backup"
+    echo "${today} : Created backup file ${backup} \n COPIED:" > $backup_report
+    cp -rv "$source_path" "~/${backup}" | awk -F"'" '{print $2}\n' >> $backup_report
+    echo "\n" >> $backup_report
     exit 0
 fi
 
-for file_name in $source_path; do
-    if [ ! -e "${backup}/$filename" ]; then 
-        cp $file_name "~/home/user/$backup"
-# c
+backup_dir="~/${backup}"
+
+touch tmp
+for filename in $source_path; do
+    if [ ! -e "$backup_dir/${filename}"]; then
+        cp "${source_path}/${filename}" "$backup_dir"
+        echo "ADD: ${filename}" >> tmp
+    else
+        old_size=`wc -c "${backup_dir}/${filename}"`
+        new_size=`wc -c "${source_path}/${filename}"`
+        if [[ $old_size != $new_size ]]; then
+            old_name="${filename}"
+            new_name="${filename}.${today}"
+            mv "${backup_dir}/${old_name}" "${backup_dir}/${new_name}"
+            mv "${source_path}/${old_name}" "${backup_dir}/${old_name}"
+            echo "UPD: ${old_name} ${new_name}" >> tmp
+        fi
+    fi
+done
+
+echo "${today} : ${source_path} was modified:" >> $backup_report
+grep "ADD:" tmp >> $backup_report
+grep "UPD:" tmp >> $backup_report
+echo "\n" >> $backup_report
+
+rm rf tmp
+
