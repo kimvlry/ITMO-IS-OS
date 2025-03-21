d167de6bdaf72205bc389029cc59c47ed2f6d069
Valerie <412963@niuitmo.ru>
2025-03-21 15:53:57 +0100

    mew

--- lab-4/untrash.sh
+++ lab-4/untrash.sh
@@ -14,3 +14,33 @@
 #    восстанавливаемого файла.
 
 #!/bin/bash
+
+log="$HOME/.trash.log"
+if [[ ! -f "$log" ]]; then
+    echo "there's no trash log"
+    exit 1
+fi
+
+echo "input name of file to restore"
+read filename
+
+while IFS= read -r line; do
+    # format: removed /path/ORIGINAL
+    #         created hard link: /path/LINK
+    if [[ "$line" == *"removed"*"$filename"* ]]; then
+        echo "$line"
+        if IFS= read -r nextline && [[ "$nextline" == *"created hard link:"* ]]; then 
+            link_name=$(echo "$line" | awk -F': ' '{print $2}')
+            echo "restore $link_name? y/n"
+            dir=$(dirname "$link_name")
+            original_name=$(basename "$link_name" | sed -E 's/_[0-9]+//')
+
+            read input 
+            if [[ "$input" == "y" ]]; then
+                echo "$original_name"
+                echo "$dir"
+            fi
+        fi
+    fi
+done < "$log"
+
