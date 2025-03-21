c9f432e4abbb49c2b579c4681926b6f14d71c6f3
Kim Valerie <412963@niuitmo.ru>
2025-03-21 14:34:50 +0300

    Update 1.sh

--- lab-3/1.sh
+++ lab-3/1.sh
@@ -15,7 +15,7 @@ mkdir /home/valerie/test 2>/dev/null && {
     touch /home/valerie/test/$timestamp;
 }
 
-ping -c 1 www.net_nikogo.ru >/dev/null 2>/dev/null || echo "$timestamp host is unreachable" >> /home/valerie/report
+ping -c 1 www.net_nikogo.ru >/dev/null || echo "$timestamp host is unreachable" >> /home/valerie/report
 
 # >2/dev/null: 2 stands for stderr
 # 2>&1: redirect stderr contents to the stdout
