bf64e9ae81429d221cef1f043819d42cec249b41
Valerie <412963@niuitmo.ru>
2025-04-05 17:26:54 +0200

    mew

---
+++ lab-5/experiment2/monitoring.sh
@@ -0,0 +1,21 @@
+# !/bin/bash
+
+N="$1"
+K="$2"
+report="$3"
+
+if [ -z "$N" ] || [ -z "$K" ] || [ -z "$report" ]; then
+    echo "pass N, K, report file name"
+    exit 
+fi
+
+for ((i=1; i<=$K; ++i)); do
+    ./newmem.bash $N & 
+    pid=$!
+    wait $pid
+done
+
+>$report
+echo -e "N = $N, K = $K\n" > $report
+dmesg | grep -iE "oom|out of memory" >> $report
+
--- lab-5/experiment2/newmem.bash
+++ lab-5/experiment2/newmem.bash
@@ -6,21 +6,13 @@ if [ -z "$1" ]; then
     exit
 fi
 
-report="report.log"
-> $report
-
-counter=0
 declare -a arr
 
 while true;  do
-    if [[ ${#arr[@]} > $N ]]; then
+    if (( ${#arr[@]} > $N )); then
         echo "arr size reached $N"
         exit
     fi
     arr+=(1 2 3 4 5 6 7 8 9 10)
-    counter=$((counter + 1))
-    if ((counter % 10000 == 0)); then 
-        echo "step: $counter, arr size: ${#arr[@]}" >> $report
-    fi
 done 
 
