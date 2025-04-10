556a4dec4a3cf1ce1a52ce0e122c7454ccfefe55
Valerie <412963@niuitmo.ru>
2025-04-10 14:24:30 +0200

    fix logs

--- lab-5/experiment1/part2/monitoring.sh
+++ lab-5/experiment1/part2/monitoring.sh
@@ -8,17 +8,19 @@ counter=0
 monitor() {
     while true; do
         top -b -n 1 | head -n 12 >> "$report"
-        sleep 1
+        sleep 5
     done
 }
 
 monitor &
 monitor_pid=$!
 
-trap "kill $monitor_pid" EXIT
+trap "kill $monitor_pid 2>/dev/null" EXIT
 
 ./mem.bash &
 pid1=$!
 ./mem2.bash &
 pid2=$!
 
+wait $pid1 $pid2
+kill $monitor_pid 2>/dev/null
